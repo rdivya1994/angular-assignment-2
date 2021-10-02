23d9f0cc6591f94d4caad359653ffe6330e69587
Divya Ravichandran <divichandran5@gmail.com>
2021-10-02 11:04:54 +0530

    added two more user images

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -9,6 +9,8 @@ export class UsersComponent implements OnInit {
 myimage1:string="assets/Images/image1.jpg";
 myimage2:string="assets/Images/image2.jpg";
 myimage3:string="assets/Images/image3.jpg";
+myimage4:string="assets/Images/image4.jpg";
+myimage5:string="assets/Images/image5.jpg";
   constructor() { }
 
   ngOnInit(): void {
